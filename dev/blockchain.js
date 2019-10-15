fe97ee635f27bc5c435e19c0c593aca3c480626f
Saurabh yadav <saurabh321.mymailbox@gmail.com>
2019-10-15 08:43:57 +0530

    Created a new Transaction and push into the block

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,18 +1,18 @@
 function Blockchain(){
     this.chain = [];
-    this.newTransaction = [];
+    this.pendingTransaction = [];
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
     const newBlock = {
         index: this.chain.length+1,
         timeStamp:Date.now(),
-        transaction:this.newTransaction,
+        transaction:this.pendingTransaction,
         nonce:nonce,
         hash: hash,
         previousBlockHash:previousBlockHash
     };
-    this.newTransaction = [];
+    this.pendingTransaction = [];
     this.chain.push(newBlock);
 
     return newBlock;
@@ -22,5 +22,19 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
 Blockchain.prototype.getLastBlock = function(){
     return this.chain[this.chain.length-1];
 }
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
+        const newTransaction = {
+            amount: amount,
+            sender:sender,
+            recipient:recipient
+        };
+
+        this.pendingTransaction.push(newTransaction);
+
+        return this.getLastBlock()['index'] + 1;
+}
+
+
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -2,6 +2,17 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock();
+// bitcoin.createNewBlock(8923239, 'DSJ7HDJD73373','G74FG47F3DY8');
 
-console.log(bitcoin);
+bitcoin.createNewBlock(8923239, 'SAURHDJD73373','GARIG47F3DY8');
+
+bitcoin.createNewTransaction(100,'BARRA','KANPUR');
+
+ bitcoin.createNewBlock(8933239, 'RAH7HDJD73373','PARFG47F3DY8');
+
+ bitcoin.createNewTransaction(300,'BARRA1','KANPUR1');
+ bitcoin.createNewTransaction(200,'BARRA2','KANPUR2');
+ bitcoin.createNewTransaction(500,'BARRA3','KANPUR3');
+
+ bitcoin.createNewBlock(8933230, 'RA27HDJD73373','PA2FG47F3DY8');
+console.log(bitcoin.chain[2]);
